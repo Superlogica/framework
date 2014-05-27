0e44c8548db56fc7a33f8c1cdfed1c16a33f74ae
Isaque <isaque@superlogica.com>
2014-05-27 17:57:52 -0300

    correção dos diretórios

--- instalar.bat
+++ instalar.bat
@@ -1,8 +1,8 @@
-mkdir Superlogica
-(new-object System.Net.WebClient).DownloadFile("https://github.com/Superlogica/framework/archive/master.zip","/Superlogica/kit.zip"); unzip kit.zip
+mkdir Superlogica;
+(new-object System.Net.WebClient).DownloadFile("https://github.com/Superlogica/framework/archive/master.zip","$pwd/Superlogica/kit.zip"); unzip "$pwd/Superlogica/kit.zip" -d "$pwd/Superlogica/"
 cd Superlogica
 ren framework-master sdk
-robocopy sdk/vagrant/* vagrant /e
+robocopy "$pwd/sdk/vagrant/"* "$pwd/vagrant" /e
 cd vagrant; vagrant box add desenv http://goo.gl/Y4aRr; vagrant init desenv http://goo.gl/Y4aRr 
 vagrant up  
 vagrant ssh
