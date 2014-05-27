9a01404c1c678ea83716f99e6e539eabaac9f36b
Isaque <isaque@superlogica.com>
2014-05-27 15:28:14 -0300

    ponto

--- instalar.bat
+++ instalar.bat
@@ -1,7 +1,7 @@
-﻿(new-object System.Net.WebClient).DownloadFile("https://github.com/Superlogica/framework/archive/master.zip","/Superlogica/kit.zip"); unzip kit.zip
-cd Superlogica
-ren framework-master sdk
-robocopy sdk/vagrant/* vagrant /e
+﻿(new-object System.Net.WebClient).DownloadFile("https://github.com/Superlogica/framework/archive/master.zip","/Superlogica/kit.zip"); unzip kit.zip;
+cd Superlogica;
+ren framework-master sdk;
+robocopy sdk/vagrant/* vagrant /e;
 cd vagrant; vagrant box add desenv http://goo.gl/Y4aRr; vagrant init desenv http://goo.gl/Y4aRr; 
 vagrant up;  
 vagrant ssh;
