98945a7b170336e48b8f06d49c9f842f64eb4093
Isaque <isaque@superlogica.com>
2014-05-28 16:27:41 -0300

    correções para subi máquina

--- instalar.ps1
+++ instalar.ps1
@@ -3,6 +3,14 @@ mkdir Superlogica;
 cd Superlogica
 ren framework-master sdk
 robocopy "$pwd/sdk/vagrant/"* "$pwd/vagrant" /e
-cd vagrant; vagrant box add desenv http://goo.gl/Y4aRr; vagrant init desenv http://goo.gl/Y4aRr 
+cd vagrant; ls;
+vagrant box add desenv http://goo.gl/Y4aRr;
+vagrant plugin install vagrant-vbguest;
+vagrant init desenv http://goo.gl/Y4aRr
+copy "$pwd/templates\Vagrantfile" "$pwd\Vagrantfile"
+$caminhoArquivo = "$pwd\Vagrantfile";
+$sharedFolder = $dirBase+"sdk";
+Add-Content $caminhoArquivo "config.vm.synced_folder '$sharedFolder' , '/home/apps'";
+Add-Content $caminhoArquivo 'end'
 vagrant up  
 vagrant ssh
