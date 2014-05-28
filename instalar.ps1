32451c9db69fe74d7479c67e3c5a33a447ae0a75
Isaque <isaque@superlogica.com>
2014-05-28 16:46:32 -0300

    correções subir maquina

--- instalar.ps1
+++ instalar.ps1
@@ -1,6 +1,7 @@
 mkdir Superlogica;
 (new-object System.Net.WebClient).DownloadFile("https://github.com/Superlogica/framework/archive/master.zip", "$pwd/Superlogica/kit.zip"); unzip "$pwd/Superlogica/kit.zip" -d "$pwd/Superlogica/";
 cd Superlogica
+$dirBase = "$pwd\"
 ren framework-master sdk
 robocopy "$pwd/sdk/vagrant/"* "$pwd/vagrant" /e
 cd vagrant; ls;
