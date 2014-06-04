902a6a85ae85d9e623c1a2a79ffb41465d71cec6
Isaque <isaque@superlogica.com>
2014-06-04 16:28:43 -0300

    escrever no arquivos

--- instalar.sh
+++ instalar.sh
@@ -3,10 +3,9 @@ cd Superlogica/vagrant;
 vagrant box add desenv http://goo.gl/Y4aRr;
 vagrant init desenv http://goo.gl/Y4aRr
 sudo curl https://raw.githubusercontent.com/Superlogica/framework/master/vagrant/templates/Vagrantfile -o Vagrantfile
-FILE_PATH_EX = PWD/Vagrantfile;
-SHARED_FOLDER = PWD/Superlogica/+"sdk";
-"config.vm.synced_folder 'SHARED_FOLDER' , '/home/apps'" >> FILE_PATH_EX;
-'end' >> FILE_PATH_EX
+SHARED_FOLDER = "$(PWD)/../sdk";
+echo "config.vm.synced_folder '${PWD}/../sdk' , '/home/apps'" >> Vagrantfile;
+echo 'end' >> Vagrantfile
 vagrant up  
 vagrant plugin install vagrant-vbguest;
 vagrant ssh default -c "sudo ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions;"
