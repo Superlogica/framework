34c97718a623209612c818995b00df35ba5a44ea
Isaque <isaque@superlogica.com>
2014-06-06 10:47:42 -0300

    mudança de ubuntu

--- instalar.ps1
+++ instalar.ps1
@@ -1,7 +1,7 @@
 mkdir Superlogica; mkdir Superlogica/vagrant; mkdir Superlogica/sdk;
 cd Superlogica/vagrant;
-vagrant box add desenv http://goo.gl/Y4aRr;
-vagrant init desenv http://goo.gl/Y4aRr
+vagrant box add desenv http://files.vagrantup.com/lucid32.box
+vagrant init desenv http://files.vagrantup.com/lucid32.box
 (new-object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/Superlogica/framework/master/vagrant/templates/Vagrantfile", "$pwd/Vagrantfile"); 
 $sharedFolder = "$pwd/../sdk";
 Add-Content "$pwd/Vagrantfile" "config.vm.synced_folder '$sharedFolder' , '/home/apps'";
