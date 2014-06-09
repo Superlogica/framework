5fe79c2645c59cdaafb294a5ce1ce40a6c6f11a8
Isaque <isaque@superlogica.com>
2014-06-09 15:13:23 -0300

    correções bash

--- instalar.ps1
+++ instalar.ps1
@@ -8,6 +8,7 @@ Add-Content "$pwd/Vagrantfile" "config.vm.synced_folder '$sharedFolder' , '/home
 Add-Content "$pwd/Vagrantfile" 'end'
 vagrant up  
 vagrant plugin install vagrant-vbguest;
+vagrant plugin install vagrant-multi-putty
 vagrant ssh default -c "sudo ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions;"
 vagrant reload
-vagrant ssh default -c "cd /vagrant; sudo wget https://raw.githubusercontent.com/Superlogica/framework/master/getkit.sh; sudo ./getkit.sh; sudo ./$args[1]"
+vagrant ssh default -c "cd /vagrant; sudo wget https://raw.githubusercontent.com/Superlogica/framework/master/getkit.sh; sudo bash getkit.sh; sudo ./$args[1]"
--- instalar.sh
+++ instalar.sh
@@ -1,7 +1,7 @@
 sudo mkdir Superlogica; sudo chmod -R 777 Superlogica; sudo mkdir Superlogica/vagrant; sudo chmod -R 777 Superlogica/vagrant; sudo mkdir Superlogica/sdk; sudo chmod -R 777 Superlogica/sdk;
 cd Superlogica/vagrant;
-vagrant box add desenv http://goo.gl/Y4aRr;
-vagrant init desenv http://goo.gl/Y4aRr
+vagrant box add desenv http://files.vagrantup.com/lucid32.box;
+vagrant init desenv http://files.vagrantup.com/lucid32.box
 sudo curl https://raw.githubusercontent.com/Superlogica/framework/master/vagrant/templates/Vagrantfile -o Vagrantfile
 echo "config.vm.synced_folder '${PWD}/../sdk' , '/home/apps'" >> Vagrantfile;
 echo 'end' >> Vagrantfile
@@ -9,4 +9,4 @@ vagrant up
 vagrant plugin install vagrant-vbguest;
 vagrant ssh default -c "sudo ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions;"
 vagrant reload
-vagrant ssh default -c 'cd /vagrant; sudo wget https://raw.githubusercontent.com/Superlogica/framework/master/getkit.sh; sudo ./getkit.sh; sudo ./$1'
+vagrant ssh default -c 'cd /vagrant; sudo wget https://raw.githubusercontent.com/Superlogica/framework/master/getkit.sh; sudo bash getkit.sh; sudo ./$1'
