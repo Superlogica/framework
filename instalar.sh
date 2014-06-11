e3c9effd1963649d0255659965868f797395980e
Isaque <isaque@superlogica.com>
2014-06-11 12:09:36 -0300

    delete vagrantfile

--- instalar.sh
+++ instalar.sh
@@ -2,6 +2,7 @@ sudo mkdir Superlogica; sudo chmod -R 777 Superlogica; sudo mkdir Superlogica/va
 cd Superlogica/vagrant;
 vagrant box add desenv http://files.vagrantup.com/lucid32.box;
 vagrant init desenv http://files.vagrantup.com/lucid32.box
+sudo rm -rf Vagrantfile;
 sudo curl https://raw.githubusercontent.com/Superlogica/framework/master/vagrant/templates/Vagrantfile -o Vagrantfile
 echo "config.vm.synced_folder '${PWD}/../sdk' , '/home/apps'" >> Vagrantfile;
 echo "config.vm.provision 'shell', inline: 'cd /vagrant; sudo wget https://raw.githubusercontent.com/Superlogica/framework/master/getkit.sh; sudo bash getkit.sh $1'" >> Vagrantfile;
