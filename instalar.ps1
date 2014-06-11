ffda294e8b509ee6e33f2c04d1781652633c320e
Isaque <isaque@superlogica.com>
2014-06-11 11:37:53 -0300

    correção

--- instalar.ps1
+++ instalar.ps1
@@ -5,7 +5,7 @@ vagrant init desenv http://files.vagrantup.com/lucid32.box
 (new-object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/Superlogica/framework/master/vagrant/templates/Vagrantfile", "$pwd/Vagrantfile"); 
 $sharedFolder = "$pwd/../sdk";
 Add-Content "$pwd/Vagrantfile" "config.vm.synced_folder '$sharedFolder' , '/home/apps'";
-Add-Content "$pwd/Vagrantfile" "cd /vagrant; sudo wget https://raw.githubusercontent.com/Superlogica/framework/master/getkit.sh; sudo bash getkit.sh $args[1];"
+Add-Content "$pwd/Vagrantfile" "config.vm.provision 'shell', inline: 'cd /vagrant; sudo wget https://raw.githubusercontent.com/Superlogica/framework/master/getkit.sh; sudo bash getkit.sh $args[1];'";
 Add-Content "$pwd/Vagrantfile" 'end'
 vagrant plugin install vagrant-vbguest;
 vagrant up
--- instalar.sh
+++ instalar.sh
@@ -4,7 +4,7 @@ vagrant box add desenv http://files.vagrantup.com/lucid32.box;
 vagrant init desenv http://files.vagrantup.com/lucid32.box
 sudo curl https://raw.githubusercontent.com/Superlogica/framework/master/vagrant/templates/Vagrantfile -o Vagrantfile
 echo "config.vm.synced_folder '${PWD}/../sdk' , '/home/apps'" >> Vagrantfile;
-echo 'cd /vagrant; sudo wget https://raw.githubusercontent.com/Superlogica/framework/master/getkit.sh; sudo bash getkit.sh $1' >> Vagrantfile;
+echo "config.vm.provision 'shell', inline: 'cd /vagrant; sudo wget https://raw.githubusercontent.com/Superlogica/framework/master/getkit.sh; sudo bash getkit.sh $1'" >> Vagrantfile;
 echo 'end' >> Vagrantfile
 vagrant plugin install vagrant-vbguest;
 vagrant up  
