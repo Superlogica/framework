3220d4eba9feddaa784a0c90d8f150cf0b83ae17
Isaque <isaque@superlogica.com>
2014-06-11 18:06:55 -0300

    correções

--- getkit.sh
+++ getkit.sh
@@ -3,5 +3,4 @@ sudo apt-get install unzip;
 cd /home/apps/; sudo wget http://github.com/Superlogica/framework/archive/master.zip; unzip -d /home/apps master.zip;
 sudo cp -f -R framework-master/* .
 sudo cp -f -R ./vagrant/* /vagrant
-cd /vagrant; sudo bash cloud-apps;
 rm -rvf framework-master; rm -rvf master.zip
