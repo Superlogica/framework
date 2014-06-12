b13e80846fc0d12b66017bb135a270fd70c18783
Isaque <isaque@superlogica.com>
2014-06-12 09:13:57 -0300

    permissão 777

--- instalar.sh
+++ instalar.sh
@@ -4,6 +4,7 @@ vagrant box add desenv http://files.vagrantup.com/lucid32.box;
 vagrant init desenv http://files.vagrantup.com/lucid32.box
 sudo rm -rf Vagrantfile;
 sudo curl https://raw.githubusercontent.com/Superlogica/framework/master/vagrant/templates/Vagrantfile -o Vagrantfile
+sudo chmod 777 Vagrantfile
 echo "config.vm.synced_folder '${PWD}/../sdk' , '/home/apps'" >> Vagrantfile;
 echo "config.vm.provision 'shell', inline: 'cd /vagrant; sudo wget https://raw.githubusercontent.com/Superlogica/framework/master/vagrant/cloud-apps; sudo bash cloud-apps --no-check-certificate;'" >> Vagrantfile;
 echo 'end' >> Vagrantfile
