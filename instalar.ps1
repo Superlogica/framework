3f33a15a3f64e8a38357587b087d7112e56ccfbe
Isaque <isaque@superlogica.com>
2014-06-17 10:29:40 -0300

    problema com a comparação

--- instalar.ps1
+++ instalar.ps1
@@ -3,14 +3,13 @@ cd Superlogica/vagrant;
 vagrant box add desenv http://files.vagrantup.com/lucid32.box
 vagrant init desenv http://files.vagrantup.com/lucid32.box
 (new-object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/Superlogica/framework/master/vagrant/templates/Vagrantfile", "$pwd/Vagrantfile"); 
-$sharedFolder = "$pwd/../sdk";
 Add-Content "$pwd/Vagrantfile" "config.vm.provision 'shell', inline: 'sudo ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions;'";
 Add-Content "$pwd/Vagrantfile" 'end'
 vagrant plugin install vagrant-vbguest;
 vagrant provision
 $modo = $args[0];
 (new-object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/Superlogica/framework/master/vagrant/templates/Vagrantfile", "$pwd/Vagrantfile"); 
-If ($modo = 'cloud-apps'){
+If ($modo == 'cloud-apps'){
 	$sharedFolder = "$pwd/../sdk";
 	Add-Content "$pwd/Vagrantfile" "config.vm.synced_folder '$sharedFolder' , '/home/apps'";
 } 
