47cb2d43ad645fda3ce8ea0b737f618f73ea21d1
Isaque <isaque@superlogica.com>
2014-06-18 14:30:56 -0300

    troca ubuntu linux

--- instalar.ps1
+++ instalar.ps1
@@ -1,8 +1,8 @@
 if((Test-Path $pwd/Superlogica/vagrant/.vagrant) -eq 0){
 	mkdir Superlogica; mkdir Superlogica/vagrant; mkdir Superlogica/sdk;
 	cd Superlogica/vagrant;
-	vagrant box add desenv http://files.vagrantup.com/lucid32.box
-	vagrant init desenv http://files.vagrantup.com/lucid32.box
+	vagrant box add desenv http://dl.dropbox.com/u/1537815/precise64.box
+	vagrant init desenv http://dl.dropbox.com/u/1537815/precise64.box
 } else {
 	cd Superlogica/vagrant;
 }
