7e4cc23ceaa03d1e77086ee8ee8281aed3c13f5b
carlossuperlogica <carlos@superlogica.com>
2014-07-22 15:18:17 -0300

    exec_script

--- vagrant/libs/lib.php
+++ vagrant/libs/lib.php
@@ -1,1 +1,9 @@
 <?
+function exec_script($script){
+    $comandos = explode("\n",$script);
+	$result = '';
+    foreach ($comandos as $comando){
+	     $result .= `{$comando} 1>&2`;
+	}	
+	return $result;
+}
