39f6b4d7d53bc8cf51da8470a4afcbeffbb4382c
carlossuperlogica <carlos@superlogica.com>
2014-07-22 17:27:37 -0300

    Update lib.php

--- vagrant/libs/lib.php
+++ vagrant/libs/lib.php
@@ -7,3 +7,20 @@ function exec_script($script){
 	}	
 	return $result;
 }
+
+
+function put_template($nome,$destino,$vars=false){
+	
+
+if (is_file("/tmp/cloud/templates/$nome")){
+   @unlink("/tmp/cloud/templates/$nome");
+}
+exec_script("cd /tmp/cloud/; sudo wget https://raw.githubusercontent.com/Superlogica/framework/master/vagrant/templates/$nome --no-check-certificate;"); 
+if (!is_file("/tmp/cloud/templates/$nome")){
+    echo("\nTemplate '$nome' não encontrado.\n\n");
+    exit(3);
+}	
+  $template = file_get_contents("/tmp/cloud/templates/$nome");
+  if (is_array($vars)) $template = strtr($template, $vars);
+  file_put_contents($destino, $template); 
+}  
