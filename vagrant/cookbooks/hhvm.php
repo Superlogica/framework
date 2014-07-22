86135111ab1978350588249fbe68d5422201da79
carlossuperlogica <carlos@superlogica.com>
2014-07-22 14:45:32 -0300

    Update hhvm.php

--- vagrant/cookbooks/hhvm.php
+++ vagrant/cookbooks/hhvm.php
@@ -1,4 +1,13 @@
-function hhvm(){
+function hhvm($maq="precise"){
 
+
+exec_script("
+wget -O - http://dl.hhvm.com/conf/hhvm.gpg.key | sudo apt-key add -
+echo deb http://dl.hhvm.com/ubuntu $maq main | sudo tee /etc/apt/sources.list.d/hhvm.list
+sudo apt-get update
+sudo apt-get install hhvm
+sudo update-rc.d hhvm defaults
+sudo /usr/share/hhvm/install_fastcgi.sh
+");
 
 }
