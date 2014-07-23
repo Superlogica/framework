76086bb801a4a8503c867e271031a511c2fb84cc
carlossuperlogica <carloscera@MacBook-Pro-de-Carlos.local>
2014-07-23 16:49:58 -0300

    idem

--- vagrant/cookbooks/zephir.php
+++ vagrant/cookbooks/zephir.php
@@ -5,15 +5,10 @@ function zephir_init(){
 
 exec_script("
 cd /tmp
-git clone https://github.com/json-c/json-c.git
-cd json-c
-sh autogen.sh
-./configure
-make && sudo make install
-cd /tmp
-git clone https://github.com/phalcon/zephir
-cd zephir
-./install -c
+cd /tmp; sudo git clone https://github.com/json-c/json-c.git
+cd /tmp/json-c; sh autogen.sh; ./configure; make && sudo make install
+cd /tmp; sudo git clone https://github.com/phalcon/zephir
+cd /tmp/zephir; ./install -c
 sudo apt-get -y install vim
 ");
 
