9a7225c323baaa2509e2b9a541be7e90c06a4650
carlossuperlogica <carloscera@MacBook-Pro-de-Carlos.local>
2014-07-24 17:07:56 -0300

    atualiza o cloud.ini

--- vagrant/cookbooks/cloudini.php
+++ vagrant/cookbooks/cloudini.php
@@ -1,8 +1,7 @@
 <?php
 
 function cloudini_init(){
-    
-    put_template("php-5-4.ini","/etc/php5/apache2/conf.d/php-5-4.ini");
+
         if (is_dir("/home/cloud/")){
            put_template("cloud.ini","/home/cloud/configs/cloud.ini");
         }
