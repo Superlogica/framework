f56d6f101b3b7187755c3caf1dd135ebee46af59
carlossuperlogica <carloscera@mbp-de-carlos.localdomain>
2014-07-31 09:47:50 -0300

    Zephir agora compila e não é mais um box

--- vagrant/cookbooks/zephir.php
+++ vagrant/cookbooks/zephir.php
@@ -4,14 +4,15 @@ function zephir_init(){
 
 
 exec_script("
-sudo cloud-init cloud
-sudo apt-get -y install git gcc make re2c php5 php5-dev libpcre3-dev
+sudo apt-get -y install git-core gcc make re2c php5 php5-dev libpcre3-dev
 cd /tmp
 cd /tmp; sudo git clone https://github.com/json-c/json-c.git
 cd /tmp/json-c; sh autogen.sh; ./configure; make && sudo make install
 cd /tmp; sudo git clone https://github.com/phalcon/zephir
 cd /tmp/zephir; ./install -c
 sudo apt-get -y install vim
+cd /home/cloud/todos-atual/library/zephir/superlogica; zephir fullclean; zephir build;
 ");
+firebird_restart();
 
 }
