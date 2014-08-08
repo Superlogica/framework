3ac9cda16db9756e2cf04b058ccad13dd8234bfd
Telma <telma.araujo@superlogica.com>
2014-08-08 17:46:58 -0300

    Update instalar.ps1

--- instalar.ps1
+++ instalar.ps1
@@ -1,8 +1,8 @@
 if((Test-Path $pwd/Superlogica/vagrant/.vagrant) -eq 0){
 	mkdir Superlogica; mkdir Superlogica/vagrant; mkdir Superlogica/sdk;
 	cd Superlogica/vagrant;
-	vagrant box add desenv http://files.vagrantup.com/lucid64.box
-	vagrant init desenv http://files.vagrantup.com/lucid64.box
+	vagrant box add desenv https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box
+	vagrant init desenv https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box
 } else {
 	cd Superlogica/vagrant;
 }
