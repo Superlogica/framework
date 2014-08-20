45c628a51e8fc291d694fd5cbdae135bc1d6728b
isaqued <isaque@superlogica.com>
2014-08-20 12:20:55 -0300

    sudo no cache

--- vagrant/cookbooks/eaccelerator.php
+++ vagrant/cookbooks/eaccelerator.php
@@ -8,7 +8,7 @@ exec_script("
 	cd /tmp/eaccelerator; tar --strip-components 1 -xf /tmp/eaccelerator/eaccelerator.tar; phpize; ./configure; make; make install;
     rm -rf /tmp/eaccelerator;
     sudo mkdir -p /var/cache/eaccelerator;
-    chmod 777 /var/cache/eaccelerator;
+    sudo chmod -R 777 /var/cache/eaccelerator;
 ");  
 
     put_template("eaccelerator.ini", "/etc/php5/conf.d/eaccelerator.ini");     
