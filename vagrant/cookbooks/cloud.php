f92752fb1fd7cb83d8598364110d64b85537f91b
isaqued <isaque@superlogica.com>
2014-08-22 11:16:16 -0300

    pasta sincronizada

--- vagrant/cookbooks/cloud.php
+++ vagrant/cookbooks/cloud.php
@@ -21,15 +21,10 @@ exec_script("
     sudo rm /etc/php5/conf.d/timezone.ini
     sudo mkdir /home/session;sudo chmod -R 777 /home/session");
     time_zone();
+	
 //eaccelerator         
-exec_script("sudo apt-get install make;
-	cd /tmp; mkdir /tmp/eaccelerator; cd eaccelerator; wget -O /tmp/eaccelerator/eaccelerator.tar https://github.com/eaccelerator/eaccelerator/tarball/master;
-	cd /tmp/eaccelerator; tar --strip-components 1 -xf /tmp/eaccelerator/eaccelerator.tar; phpize; ./configure; make; make install;
-    rm -rf /tmp/eaccelerator;
-    sudo mkdir -p /var/cache/eaccelerator;
-    chmod 777 /var/cache/eaccelerator;
-    sudo cp {$conf['basedir']}/templates/eaccelerator.ini /etc/php5/conf.d/;
-");  
+exec_script("cloud-init eaccelerator");
+
 //git clone
 //exec_script("cd /home; sudo git clone git@github.com:Superlogica/cloud.git"); 
 //firewall
