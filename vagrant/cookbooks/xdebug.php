629aba5af840e9c522697bad88620abdb7051f87
Alan Tavares <alan.t.chaves@gmail.com>
2014-08-28 08:08:32 -0300

    melhorias no cookbook do xdebug

--- vagrant/cookbooks/xdebug.php
+++ vagrant/cookbooks/xdebug.php
@@ -1,7 +1,14 @@
 <?php
 
 function xdebug_init(){
+    
+    put_template("xdebug.ini", "/etc/php5/conf.d/xdebug.ini");
+
+    $diretorioExtensoes = ini_get('extension_dir');    
+    $xdebugIni = 'zend_extension='.rtrim($diretorioExtensoes,DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR.'xdebug.so';
+
     exec_script("
+        sed -i '1i".$xdebugIni."' /etc/php5/conf.d/xdebug.ini;
         cd /tmp; 
         mkdir /tmp/xdebug; 
         cd xdebug;
@@ -15,6 +22,7 @@ function xdebug_init(){
         sudo mkdir -p /var/log/xdebug;
         sudo mkdir -p /var/log/xdebug/profiler/;
         sudo chmod -R 777 /var/log/xdebug;
+        sudo /etc/init.d/apache2 restart;
     ");
-    put_template("xdebug.ini", "/etc/php5/conf.d/xdebug.ini");     
+
 }
