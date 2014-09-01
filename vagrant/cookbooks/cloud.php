62c564a8f42970e42eba5ca3625a3e3d2c2f77d7
isaqued <isaque@superlogica.com>
2014-09-01 11:26:23 -0300

    php 5.5

--- vagrant/cookbooks/cloud.php
+++ vagrant/cookbooks/cloud.php
@@ -38,8 +38,12 @@ exec_script("
 
 
 //ativar app no apache
- exec_script(" sudo rm /etc/apache2/sites-enabled/*; sudo ln -s /home/cloud/conf/cloud.superlogica.com /etc/apache2/sites-enabled/001cloud
-            sudo rm -Rf /var/www
+if ($php_version=="5.5")
+	exec_script("sudo rm /etc/apache2/sites-enabled/*; sudo ln -s /home/cloud/conf/cloud.superlogica.com_php55 /etc/apache2/sites-enabled/001cloud.conf");
+else
+	exec_script("sudo rm /etc/apache2/sites-enabled/*; sudo ln -s /home/cloud/conf/cloud.superlogica.com /etc/apache2/sites-enabled/001cloud");
+
+ exec_script("sudo rm -Rf /var/www
             sudo ln -s /home/cloud  /var/www
             sudo cloud-init cloudini
             sudo cloud-init phpini
