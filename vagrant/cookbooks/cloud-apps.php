86b48f0ac1486a74be7fd950eb28ea65d1784ace
isaqued <isaque@superlogica.com>
2014-11-04 16:23:45 -0200

    Update cloud-apps.php

--- vagrant/cookbooks/cloud-apps.php
+++ vagrant/cookbooks/cloud-apps.php
@@ -1,10 +1,5 @@
 <?php
 
-require_once 'lib.php'; 
-require_once realpath(dirname(__FILE__)).'/../actions.php';
-require_once realpath(dirname(__FILE__)).'/../conf.php'; 
-
-global $conf;
 
 exec_script("
     sudo apt-get update
@@ -17,8 +12,8 @@ exec_script("
 exec_script("
     sudo apt-get -y install apache2 libapache2-mod-php5 php5-mysql php5-mcrypt lynx lynx-cur php5-curl php5-dev php5-gd php5-mcrypt php5-memcache php5-memcached php5-mysql;  
     sudo apt-get install phpmyadmin; 
-	sudo su;
-	sudo echo 'Include /etc/phpmyadmin/apache.conf' >> /etc/apache2/apache2.conf;	
+    sudo su;
+    sudo echo 'Include /etc/phpmyadmin/apache.conf' >> /etc/apache2/apache2.conf;	
     sudo a2enmod ssl;
     sudo a2enmod rewrite;
     sudo apt-get -y install apachetop;
@@ -26,21 +21,22 @@ exec_script("
     sudo apt-get -y install tz-brasil;
     sudo dpkg-reconfigure tzdata;
     sudo chmod 444 /etc/init.d/postfix
-    sudo cp {$conf['basedir']}/templates/superlogica.ini /etc/php5/apache2/conf.d/;
-    cd /usr/local; sudo wget http://downloads2.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz; sudo tar xzf ioncube_loaders_lin_x86-64.tar.gz;
-    sudo cp {$conf['basedir']}/templates/ioncube.ini /etc/php5/apache2/conf.d/;    
+    sudo cloud-init cloudini 'plataforma'
+    sudo cloud-init phpini
     sudo rm /etc/php5/conf.d/timezone.ini
-	sudo mkdir /home/apps/session; sudo chmod -R 777 /home/apps/session");
-
+    sudo mkdir /home/apps/session; sudo chmod -R 777 /home/apps/session");
+	//fazer ioncube depois
+   // cd /usr/local; sudo wget http://downloads2.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz; sudo tar xzf ioncube_loaders_lin_x86-64.tar.gz;
+   // sudo cp {$conf['basedir']}/templates/ioncube.ini /etc/php5/apache2/conf.d/;  
 //eaccelerator         
-exec_script("
-    cd /tmp; mkdir /tmp/eaccelerator; cd eaccelerator; wget -O /tmp/eaccelerator/eaccelerator.tar https://github.com/eaccelerator/eaccelerator/tarball/master;
-    cd /tmp/eaccelerator; tar --strip-components 1 -xf /tmp/eaccelerator/eaccelerator.tar; phpize; ./configure; make; make install;
-    rm -rf /tmp/eaccelerator;
-    sudo mkdir -p /var/cache/eaccelerator;
-    chmod 777 /var/cache/eaccelerator;
-    sudo cp {$conf['basedir']}/templates/eaccelerator.ini /etc/php5/conf.d/;
-");  
+//exec_script("
+//    cd /tmp; mkdir /tmp/eaccelerator; cd eaccelerator; wget -O /tmp/eaccelerator/eaccelerator.tar https://github.com/eaccelerator/eaccelerator/tarball/master;
+//    cd /tmp/eaccelerator; tar --strip-components 1 -xf /tmp/eaccelerator/eaccelerator.tar; phpize; ./configure; make; make install;
+//    rm -rf /tmp/eaccelerator;
+//    sudo mkdir -p /var/cache/eaccelerator;
+//    chmod 777 /var/cache/eaccelerator;
+//    sudo cp {$conf['basedir']}/templates/eaccelerator.ini /etc/php5/conf.d/;
+//");  
 
 // exec_script("sudo ln -s /home/plataforma/public /home/apps/public");
 
