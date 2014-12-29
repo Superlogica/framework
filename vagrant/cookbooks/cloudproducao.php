6f2bca9d9e507f97dd7ff7ee7d65ae492c7d6f5c
carlossuperlogica <carlos@superlogica.com>
2014-12-29 18:15:48 -0200

    Update cloudproducao.php

--- vagrant/cookbooks/cloudproducao.php
+++ vagrant/cookbooks/cloudproducao.php
@@ -3,5 +3,6 @@ function cloudproducao_init(){
 
 exec_script("sudo cloud-init cloud
              cd /home; sudo git clone git@github.com:Superlogica/cloud.git
-             sudo cloud-init cloud");
+             sudo cloud-init cloud
+             sudo cloud-init cloudini cloudteste");
 }
