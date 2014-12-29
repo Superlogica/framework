7cbe27fc9c470dddc7acb5a69d755fab27592aed
carlossuperlogica <carlos@superlogica.com>
2014-12-29 14:12:07 -0200

    Create cloudproducao.php

---
+++ vagrant/cookbooks/cloudproducao.php
@@ -0,0 +1,7 @@
+
+function cloudproducao_init(){
+
+exec_script("sudo cloud-init cloud
+             cd /home; sudo git clone git@github.com:Superlogica/cloud.git
+             sudo cloud-init cloud");
+}
