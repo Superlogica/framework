17b2a4436722589c5268e4943d0e40a6cab40b34
carlossuperlogica <carlos@superlogica.com>
2014-12-29 18:26:42 -0200

    Update cloud.php

--- vagrant/cookbooks/cloud.php
+++ vagrant/cookbooks/cloud.php
@@ -46,7 +46,7 @@ exec_script("
 if ($php_version=="5.5")
 	exec_script("sudo rm /etc/apache2/sites-enabled/*; sudo ln -s /home/cloud/conf/cloud.superlogica.com_php55 /etc/apache2/sites-enabled/001cloud.conf");
 else
-	exec_script("sudo rm /etc/apache2/sites-enabled/*; sudo ln -s /home/cloud/conf/cloud.superlogica.com /etc/apache2/sites-enabled/001cloud");
+	exec_script("sudo rm /etc/apache2/sites-enabled/*; sudo ln -s /home/cloud/conf/cloud.superlogica.com /etc/apache2/sites-enabled/001cloud.conf");
 
  exec_script("sudo rm -Rf /var/www
             sudo ln -s /home/cloud  /var/www
