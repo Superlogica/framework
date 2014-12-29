e6943efc4dbae5e9aa4cf8c75f39da59f3dfc661
carlossuperlogica <carlos@superlogica.com>
2014-12-29 18:14:51 -0200

    Update cloudini.php

--- vagrant/cookbooks/cloudini.php
+++ vagrant/cookbooks/cloudini.php
@@ -5,6 +5,10 @@ function cloudini_init($ambiente="cloud"){
         if ($ambiente == "cloud"){
            put_template("cloud.ini","/home/cloud/configs/cloud.ini");
         }
+        if ($ambiente == "cloudteste"){
+           put_template("cloudteste.ini","/home/cloud/configs/cloud.ini");
+        }        
+        
 		
         if ($ambiente == "plataforma"){
             put_template("cloud-apps.ini","/home/plataforma/library/Application/Configs/cloud.ini");
