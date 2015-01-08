2da0274a1125bea6ba19bf69dabbb7f4c69d9b01
carlossuperlogica <carlos@superlogica.com>
2015-01-08 10:16:36 -0200

    Create cloudtesteaws.php

---
+++ vagrant/cookbooks/cloudtesteaws.php
@@ -0,0 +1,9 @@
+<?php
+function cloudtesteaws_init(){
+exec_script("sudo cloud-init cloud
+             cd /home; sudo git clone git@github.com:Superlogica/cloud.git
+             sudo cloud-init cloud
+             sudo cloud-init cloudini cloudteste
+             sudo cloud-init fb21
+             sudo /home/cloud/conf/deploy.sh");
+}
