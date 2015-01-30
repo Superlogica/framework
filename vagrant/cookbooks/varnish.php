fe9aca9aeefa9297a59a332f87be065634aeeb0e
isaqued <isaque@superlogica.com>
2015-01-30 17:13:12 -0200

    varnish

--- vagrant/cookbooks/varnish.php
+++ vagrant/cookbooks/varnish.php
@@ -6,13 +6,18 @@ function varnish_init(){
 exec_script("
 	sudo apt-get -y install varnish  
 	sudo rm  /etc/default/varnish  
-	put_template('etc-default-varnish','/etc/default/varnish');
+");
+
+put_template('etc-default-varnish','/etc/default/varnish');
+
+exec_script("
 	sudo rm /etc/varnish/default.vcl
 	sudo ln -s /home/cloud/conf/varnish_sem_redirec_cron.conf /etc/varnish/default.vcl
 	sudo rm  /etc/apache2/ports.conf  
-	put_template('apache-port-8080.conf','/etc/apache2/ports.conf');	
 ");
 
-put_template("hhvm_proxy_fcgi.conf", "/etc/apache2/mods-available/hhvm_proxy_fcgi.conf");
+put_template('apache-port-8080.conf','/etc/apache2/ports.conf');	
+
+
 
 }
