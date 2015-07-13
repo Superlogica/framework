beb8a0928f51492221dd0e329328a0f8cacce25d
Alan Tavares <alan.t.chaves@gmail.com>
2015-07-13 14:50:56 -0300

    correção do pecl

--- vagrant/cookbooks/mongoclient.php
+++ vagrant/cookbooks/mongoclient.php
@@ -5,7 +5,7 @@ function mongoclient_init(){
     put_template("mongoclient.ini", "/etc/php5/conf.d/mongoclient.ini");
 
     exec_script("
-        sudo apt-get install php-pear;
+        sudo apt-get -f install default-mta php5-cli php5-dev php-pear;
         sudo pecl install mongo;
         sudo /etc/init.d/apache2 restart;
     ");
