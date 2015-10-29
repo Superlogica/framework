f56b0d471b9f0870213b1f5b1814ac48dd2e50b5
leandrosuper <leandro.panhan@superlogica.com>
2015-10-29 12:19:06 -0200

    Update cloudteste55.php

--- vagrant/cookbooks/cloudteste55.php
+++ vagrant/cookbooks/cloudteste55.php
@@ -1,6 +1,6 @@
 <?php
 function cloudteste_init(){
 exec_script("sudo cloud-init cloud
-             sudo cloud-init fb21
+             sudo cloud-init fb25
              sudo cloud-init xdebug");
 }
