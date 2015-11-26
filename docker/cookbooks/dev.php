877870c67b7a2be953f1c62beb6e383bf86df938
marlongb <marlon.borges.superlogica@gmail.com>
2015-11-26 08:51:48 -0200

    Correção script short_tag

--- docker/cookbooks/dev.php
+++ docker/cookbooks/dev.php
@@ -3,12 +3,12 @@
 function dev_init(){
 
 
-	exec_script("sudo chmod 777 -R /etc/php5 
-		sudo sed -i "s/;date.timezone =/date.timezone = America\/Sao_Paulo/" /etc/php5/cli/php.ini \
-		&& sed -i "s/;date.timezone =/date.timezone = America\/Sao_Paulo/" /etc/php5/fpm/php.ini \
-		&& sed -i "s/short_open_tag = Off/short_open_tag = On/" /etc/php5/cli/php.ini \
-		&& sed -i "s/short_open_tag = Off/short_open_tag = On/" /etc/php5/fpm/php.ini \
-		&& sed -i "s/short_open_tag = Off/short_open_tag = On/" /etc/php5/apache2/php.ini
+	exec_script("sudo chmod 777 -R /etc/php5
+	sudo sed -i "s/;date.timezone =/date.timezone = America\/Sao_Paulo/" /etc/php5/cli/php.ini \
+    && sed -i "s/;date.timezone =/date.timezone = America\/Sao_Paulo/" /etc/php5/fpm/php.ini \
+    && sed -i "s/short_open_tag = On/short_open_tag = Off/" /etc/php5/cli/php.ini \
+    && sed -i "s/short_open_tag = On/short_open_tag = Off/" /etc/php5/fpm/php.ini \
+    && sed -i "s/short_open_tag = On/short_open_tag = Off/" /etc/php5/apache2/php.ini
 		sudo /etc/init.d/apache2 restart
 		sudo chmod 777 -R /home/cloud 
 		sudo chmod 777 -R /home/cloud-db
