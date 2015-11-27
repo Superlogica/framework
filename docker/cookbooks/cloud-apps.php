88e9a289af6f1ea859d64774e79a6a44c9a79493
marlongb <marlon.borges.superlogica@gmail.com>
2015-11-27 17:19:29 -0200

    Correção permissao apache2

--- docker/cookbooks/cloud-apps.php
+++ docker/cookbooks/cloud-apps.php
@@ -3,6 +3,7 @@
 exec_script("echo '-----Configurando apps-----'
     sudo mkdir /home/apps/session; sudo chmod -R 777 /home/apps/session
     sudo ln -s /home/apps/conf/apps.local.linux /etc/apache2/sites-enabled/001apps.conf
+    sudo chmod 777 -R /etc/apache2/sites-enabled
     sudo ln -s /home/plataforma/library /home/apps/library
     sudo chmod 777 -R /home/apps/public/scripts/min
 ");
