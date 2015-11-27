d728db9b8586bb6e084eca6eecdd8437b2009588
marlongb <marlon.borges.superlogica@gmail.com>
2015-11-26 22:14:15 -0200

    FlameRobin (Manipular base firebird)

--- docker/cookbooks/dev.php
+++ docker/cookbooks/dev.php
@@ -3,9 +3,12 @@
 function dev_init(){
 
 
-	exec_script("sudo chmod 777 -R /etc/php5
+	exec_script("echo "-----Aplicando permissoes----"
+		sudo chmod 777 -R /etc/php5
 		sudo chmod 777 -R /home/cloud
 		sudo chmod 777 -R /home/cloud-db
+		echo "----Instalando FlameRobin----"
+		sudo apt-get install flamerobin
 	");
 
 /*
