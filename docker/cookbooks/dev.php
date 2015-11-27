2ae369db358636cfab17ba5ace02af286c4c4f4e
oliveiraeduardo <eduardo.oliveira@superlogica.com>
2015-11-27 12:32:53 -0200

    Melhorias cookbook Dev

--- docker/cookbooks/dev.php
+++ docker/cookbooks/dev.php
@@ -5,22 +5,32 @@ function dev_init(){
 
 	exec_script("
 		sudo chmod 777 -R /etc/php5
+		sudo chmod 777 -R /etc/apache2
 		sudo chmod 777 -R /home/cloud
 		sudo chmod 777 -R /home/cloud-db
 		echo '------Instalando FlameRobin------'
 		sudo apt-get install flamerobin
 		echo '------Instalando Synaptic(Gerenciador de Pacotes)------'
 		sudo apt-get install synaptic
+		echo '------Instalando Google Chromme------'
+		sudo apt-get update
+		sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+	 	sudo dpkg -i google-chrome-stable_current_amd64.deb
+	 	sudo apt-get -f install
+		echo '------Instalando SmartGit------'
+		sudo apt-get update
+		sudo add-apt-repository ppa:eugenesan/ppa
+    	sudo apt-get update
+	  	sudo apt-get install smartgithg
+	  	echo '------MySql Workbench------'
+	  	sudo apt-get update
+		sudo dpkg -i mysql-apt-config_0.5.3-1_all.deb
+		sudo apt-get update
+		sudo apt-get install mysql-workbench-community
+		echo '------Notepad++------'
+		sudo apt-get update
+		sudo add-apt-repository ppa:notepadqq-team/notepadqq
+		sudo apt-get update
+		sudo apt-get install notepadqq
 	");
-
-/*
-	sudo sed -i "s/;date.timezone =/date.timezone = America\/Sao_Paulo/" /etc/php5/cli/php.ini && sed -i "s/;date.timezone =/date.timezone = America\/Sao_Paulo/" /etc/php5/fpm/php.ini 
-	sudo sed -i "s/short_open_tag = On/short_open_tag = Off/" /etc/php5/cli/php.ini
-	sudo sed -i "s/short_open_tag = On/short_open_tag = Off/" /etc/php5/fpm/php.ini
-	sudo sed -i "s/short_open_tag = On/short_open_tag = Off/" /etc/php5/apache2/php.ini
-		sudo /etc/init.d/apache2 restart
- */
-
-
-
 }
