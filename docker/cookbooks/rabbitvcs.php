f015b7b35a951011ad300cae47d0154dbe3af9b5
oliveiraeduardo <eduardo.oliveira@superlogica.com>
2015-11-27 15:20:57 -0200

    CookBook rabbitvcs

---
+++ docker/cookbooks/rabbitvcs.php
@@ -0,0 +1,12 @@
+<?php
+
+function rabbitvcs_init(){
+
+	exec_script("
+		echo '------Rabbitvcs------'
+		sudo apt-get update
+		sudo add-apt-repository ppa:rabbitvcs/ppa
+		sudo apt-get update
+		sudo apt-get install rabbitvcs-cli rabbitvcs-core rabbitvcs-gedit rabbitvcs-nautilus3
+	");
+}
