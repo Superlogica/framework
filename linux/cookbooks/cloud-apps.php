2c6e4266a43fae180581405212e1642c8622fae4
marlongb <marlon.borges.superlogica@gmail.com>
2015-11-28 11:23:26 -0200

    Correção arquivo hosts

--- linux/cookbooks/cloud-apps.php
+++ linux/cookbooks/cloud-apps.php
@@ -1,7 +1,7 @@
 <?php
 
 exec_script(
-	"echo '-----Configurando apps-----'
+	"echo '-----------Configurando Apps-----------'
     sudo mkdir /home/apps/session
     sudo chmod -R 777 /home/apps/session
     sudo ln -s /home/apps/conf/apps.local.linux /etc/apache2/sites-enabled/001apps.conf
@@ -10,6 +10,10 @@ exec_script(
     sudo chmod 777 -R /home/apps/library
     sudo mkdir /home/apps/public/scripts/min
     sudo chmod 777 -R /home/apps/public/scripts/min
+    sudo -- sh -c 'echo 127.0.0.1      apps.superlogica.com.br >> /etc/hosts'
+    sudo -- sh -c 'echo Servername localhost >> /etc/apache2/apache2.conf'
+    sudo echo '---------Reiniciando Apache---------'
+    sudo /etc/init.d/apache2 restart
 ");
 // echo 'Include /etc/phpmyadmin/apache.conf' >> /etc/apache2/apache2.conf; 
 
