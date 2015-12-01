3962457d67f9b73595c31281fffbf264c40fb233
eduardo.oliveira@superlogica.com <eduardo.oliveira@superlogica.com>
2015-12-01 10:11:11 -0200

    Semelhante ao TortoiseGit

--- linux/cookbooks/rabbitvcs.php
+++ linux/cookbooks/rabbitvcs.php
@@ -5,6 +5,8 @@ function rabbitvcs_init(){
 	exec_script("
 		echo '------Rabbitvcs------'
 		sudo apt-get update
+		sudo apt-get install git
+		sudo apt-get update
 		sudo add-apt-repository ppa:rabbitvcs/ppa
 		sudo apt-get update
 		sudo apt-get install rabbitvcs-cli rabbitvcs-core rabbitvcs-gedit rabbitvcs-nautilus3
