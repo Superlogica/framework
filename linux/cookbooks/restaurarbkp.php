65728a52b9a2bdeae505f15c0f1164afd22c296d
marlongb <marlon.borges.superlogica@gmail.com>
2015-12-14 15:54:30 -0200

    Correção no caminho do backup

--- linux/cookbooks/restaurarbkp.php
+++ linux/cookbooks/restaurarbkp.php
@@ -8,7 +8,7 @@ function restaurarbkp_init(){
 		sudo mkdir /home/backup/
 		sudo chmod 777 -R /home/backup
 		echo '------Iniciando restauracao------'
-		gbak -create -v -user sysdba -password masterkey /home/cloud-db/backup.gbk 127.0.0.1:/home/cloud-db/restauracao-001.fdb
+		gbak -create -v -user sysdba -password masterkey /home/backup/backup.gbk 127.0.0.1:/home/cloud-db/restauracao-001.fdb
 		sudo chmod 777 -R /home/cloud-db
 	");
 }
