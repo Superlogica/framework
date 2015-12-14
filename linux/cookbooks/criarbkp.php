28482117e23a9bbc249f69f3c2b2c8c94d03d021
eduardo.oliveira@superlogica.com <eduardo.oliveira@superlogica.com>
2015-12-14 16:09:18 -0200

    Criar e restaurar backup linux

---
+++ linux/cookbooks/criarbkp.php
@@ -0,0 +1,14 @@
+<?php
+
+function criarbkp_init(){
+
+
+	exec_script("
+		echo '------Tentando criar a pasta Backup------'
+		sudo mkdir /home/cloud-db/backup
+		sudo chmod 777 -R /home/cloud-db/backup
+		echo '------Iniciando criação------'
+		gbak -backup -v -garbage -limbo -user sysdba -password masterkey /home/cloud-db/backup/base.fdb 127.0.0.1:/home/cloud-db/backup/backup.gbk
+		sudo chmod 777 -R /home/cloud-db
+	");
+}
--- linux/cookbooks/smartgit.php
+++ linux/cookbooks/smartgit.php
@@ -9,17 +9,19 @@ function smartgit_init(){
 		sudo mkdir /home/framework
 		sudo mkdir /home/apps
 		sudo mkdir /home/plataforma
-		sudo mkdir /home/backup
+		sudo mkdir /home/cloud-db
+		sudo mkdir /home/cloud-db/backup
 		sudo chmod -R 777 /home/cloud
 		sudo chmod -R 777 /home/framework
 		sudo chmod -R 777 /home/apps
 		sudo chmod -R 777 /home/plataforma
-		sudo chmod -R 777 /home/backup
+		sudo chmod -R 777 /home/cloud-db
+		sudo chmod -R 777 /home/cloud-db/backup
 		sudo apt-get update
 		sudo apt-get install -y git-core
 		sudo apt-get update
 		sudo add-apt-repository ppa:eugenesan/ppa
-    sudo apt-get update
-	  sudo apt-get install smartgithg
+    	sudo apt-get update
+	  	sudo apt-get install smartgithg
 	");
 }
