a4823f1aab8d069ec68ee197be436c74de2ba109
eduardo.oliveira@superlogica.com <eduardo.oliveira@superlogica.com>
2015-12-18 09:45:00 -0200

    Gerar log ao restaurar backup

--- linux/cookbooks/restaurarbkp.php
+++ linux/cookbooks/restaurarbkp.php
@@ -8,7 +8,7 @@ function restaurarbkp_init(){
 		sudo mkdir /home/cloud-db/backup
 		sudo chmod 777 -R /home/cloud-db/backup
 		echo '------Iniciando restauracao------'
-		gbak -create -v -user sysdba -password masterkey /home/cloud-db/backup/backup.gbk 127.0.0.1:/home/cloud-db/restauracao-001.fdb
+		gbak -create -v -user sysdba -password masterkey /home/cloud-db/backup/backup.gbk 127.0.0.1:/home/cloud-db/restauracao-001.fdb 2>> /home/cloud-db/backup/log-restauracao.txt
 		sudo chmod 777 -R /home/cloud-db
 	");
 }
