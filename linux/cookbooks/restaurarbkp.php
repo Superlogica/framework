13feedbfa8a5a5cbd246a0d5da3fa2292ba6bb93
eduardo.oliveira@superlogica.com <eduardo.oliveira@superlogica.com>
2015-12-23 14:56:59 -0200

    Teste correção restaurar bkp linux

--- linux/cookbooks/restaurarbkp.php
+++ linux/cookbooks/restaurarbkp.php
@@ -8,7 +8,7 @@ function restaurarbkp_init(){
 		sudo mkdir /home/cloud-db/backup
 		sudo chmod 777 -R /home/cloud-db/backup
 		echo '------Iniciando restauracao------'
-		gbak -create -v -user sysdba -password masterkey /home/cloud-db/backup/backup.gbk 127.0.0.1:/home/cloud-db/restauracao-001.fdb 2>> /home/cloud-db/backup/log-restauracao.txt
+		gbak -create -fix_fss_metadata win1251 -fix_fss_data win1251 -v -user sysdba -password masterkey /home/cloud-db/backup/backup.gbk 127.0.0.1:/home/cloud-db/restauracao-001.fdb 2>> /home/cloud-db/backup/log-restauracao.txt
 		sudo chmod 777 -R /home/cloud-db
 	");
 }
