aba378bff22df8fa9b1173d03aca51645d736c6e
jeanrodrigues <jean.rodrigues@superlogica.com>
2016-04-14 14:50:55 -0300

    correção geração chave publica

--- linux/cookbooks/chavepublica.php
+++ linux/cookbooks/chavepublica.php
@@ -1,15 +1,9 @@
 <?php
-
 function chavepublica_init(){
-
-
 	exec_script("
 		sudo wget https://raw.githubusercontent.com/Superlogica/framework/master/linux/templates/ssh_config -O /etc/ssh/ssh_config
                 ssh-keygen -t dsa -N ''
-                cd /home/$USER/.ssh
-                echo 'DIGITE SEU NOME'
-                read nome_user
-                cp id_dsa.pub $nome_user.pub
-                smbclient //SLNAS2/chaves -c 'put $nome_user.pub'               
+                cp \$HOME\/.ssh/id_dsa.pub \$USER\.pub
+                smbclient //SLNAS2/chaves -c 'put \$USER\.pub'              
 	");
 }
