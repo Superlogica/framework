992ace1f62e36a8798d2f9d8ff9cfb6764e843b7
jeanrodrigues <jean.rodrigues@superlogica.com>
2016-04-15 15:49:28 -0300

    correção

--- linux/cookbooks/chavepublica.php
+++ linux/cookbooks/chavepublica.php
@@ -1,8 +1,10 @@
 <?php
 function chavepublica_init(){
 	exec_script("
-		sudo wget https://raw.githubusercontent.com/Superlogica/framework/master/linux/templates/ssh_config -O /etc/ssh/ssh_config;
-                ssh-keygen -t dsa -N '';
+		sudo wget https://raw.githubusercontent.com/Superlogica/framework/master/linux/templates/ssh_config -O /etc/ssh/ssh_config");
+
+	exec_script("
+		ssh-keygen -t dsa -N '';
                 cp \$HOME\/.ssh/id_dsa.pub \$USER\.pub");
-                echo "PRESSIONE ENTER PARA CONTINUAR";exec_script("smbclient //SLNAS2/chaves -c 'put '\$USER'.pub'");
+                echo "PRESSIONE ENTER PARA CONTINUAR";exec("smbclient //SLNAS2/chaves -c 'put '\$USER'.pub'");
 }       
