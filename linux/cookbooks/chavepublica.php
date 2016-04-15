8c4fbc69c7baf5209270e2ba7e3123ce73dc25f9
jeanrodrigues <jean.rodrigues@superlogica.com>
2016-04-15 11:16:15 -0300

    alterações gerar chave publica2

--- linux/cookbooks/chavepublica.php
+++ linux/cookbooks/chavepublica.php
@@ -2,7 +2,7 @@
 function chavepublica_init(){
 	exec_script("
 		sudo wget https://raw.githubusercontent.com/Superlogica/framework/master/linux/templates/ssh_config -O /etc/ssh/ssh_config
-                sudo wget https://raw.githubusercontent.com/Superlogica/framework/master/linux/templates/chavepublica.sh -O \$HOME\
+                sudo wget https://raw.githubusercontent.com/Superlogica/framework/master/linux/templates/chavepublica.sh -O chavepublica.sh
                 sudo chmod +x chavepublica.sh;
                 sudo ./chavepublica.sh;   
 	");
