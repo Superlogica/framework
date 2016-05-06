a37bc5f6dcb2389145220e905f65dcf2184fb765
carlossuperlogica <carlos@superlogica.com>
2016-05-06 16:50:18 -0300

    SendMail tem várias falhas de segurança

--- linux/cookbooks/localserver.php
+++ linux/cookbooks/localserver.php
@@ -11,7 +11,6 @@ function localserver_init(){
         apt-get install fail2ban
         sudo apt-get install nginx
         apt-get install iptables-persistent
-        apt-get install sendmail
         iptables -A INPUT -i lo -j ACCEPT
         iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
         iptables -A INPUT -p tcp --dport 22 -j ACCEPT
