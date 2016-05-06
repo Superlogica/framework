920e5cb443aa01a9e3128f90a80a4824f5fd054e
carlossuperlogica <carlos@superlogica.com>
2016-05-06 19:27:20 -0300

    Update phpini.php

--- linux/cookbooks/phpini.php
+++ linux/cookbooks/phpini.php
@@ -1,8 +1,11 @@
 <?php
 
 function phpini_init(){
-    
-    put_template("php-5-4.ini","/etc/php5/apache2/conf.d/php-5-4.ini");
+    if ( trim(shell_exec ("lsb_release -rs")) == '16.04' ){
+        put_template("php-5-4.ini","/etc/php5/apache2/conf.d/php-5-4.ini");
+    } else {
+        put_template("php-7-0.ini","/etc/php7.0/apache2/conf.d/php-7-0.ini");
+    }    
     apache_restart();
 }
 
