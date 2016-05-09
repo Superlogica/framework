04c764ef668c354f3a7089f0ce03b1243c585194
carlossuperlogica <carlos@superlogica.com>
2016-05-09 13:29:45 -0300

    Update cloudteste.php

--- linux/cookbooks/cloudteste.php
+++ linux/cookbooks/cloudteste.php
@@ -4,21 +4,21 @@ function cloudteste_init(){
 
 
 exec_script("
-						cd /home/cloud
-						git config core.fileMode false
-						cd /home/framework
-						git config core.fileMode false
-						cd /home/apps
-						git config core.fileMode false
-						cd /home/plataforma
-						git config core.fileMode false
-						sudo cloud-init cloud
-            sudo cloud-init fb25
-            sudo cloud-init dev
-            sudo cloud-init cloud-apps
-            sudo cloud-init mysql-apps
-            echo '-------------PROCESSO FINALIZADO-------------'
-            echo '---- CLOUD ACESSE http://localhost:3059/ ----'
-            echo '----- APPS ACESSE http://localhost:8080/ ----'"
+	cd /home/cloud
+	git config core.fileMode false
+	cd /home/framework
+	git config core.fileMode false
+	cd /home/apps
+	git config core.fileMode false
+	cd /home/plataforma
+	git config core.fileMode false
+	sudo cloud-init cloud
+        sudo cloud-init fb25
+        sudo cloud-init dev
+        sudo cloud-init cloud-apps
+        sudo cloud-init mysql-apps
+        echo '-------------PROCESSO FINALIZADO-------------'
+        echo '---- CLOUD ACESSE http://localhost:3059/ ----'
+        echo '----- APPS ACESSE http://localhost:8080/ ----'"
             );
 }
