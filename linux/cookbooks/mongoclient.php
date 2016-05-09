72dc0e076f716e58c652dde599ea2e08f9ce16ca
carlossuperlogica <carlos@superlogica.com>
2016-05-09 13:50:00 -0300

    Update mongoclient.php

--- linux/cookbooks/mongoclient.php
+++ linux/cookbooks/mongoclient.php
@@ -2,12 +2,21 @@
 
 function mongoclient_init(){
     
-    put_template("mongoclient.ini", "/etc/php5/conf.d/mongoclient.ini");
+  if ( trim(shell_exec ("lsb_release -rs")) == '16.04' ){
+	 $php_version= '7.0';	
+  } else{
+  	$php_version= '5';	
+  }    
+    
+    
+    put_template("mongoclient.ini", "/etc/php{$php_version}/conf.d/mongoclient.ini");
+    
+
 
     exec_script("
         sudo apt-get install default-mta;
         sudo apt-get install php-pear
-        sudo apt-get install php5-dev;
+        sudo apt-get install php{$php_version}-dev;
         sudo pecl install mongo;
         sudo /etc/init.d/apache2 restart;
     ");
