a1fec1b3ff1d4a332f233ad1796572b44a2dad63
carlossuperlogica <carlos@superlogica.com>
2016-05-16 12:20:09 -0300

    blackfire

---
+++ linux/cookbooks/blackfire.php
@@ -0,0 +1,27 @@
+<?php
+function blackfire_init(){
+    echo "
+          **********************************************************
+                                 BLACKFIRE
+          **********************************************************
+          Para conhecer suas credenciais, visite:
+          https://blackfire.io/docs/up-and-running/installation
+          **********************************************************
+          ";
+          
+          
+    exec_script("
+    
+    wget -O - https://packagecloud.io/gpg.key | sudo apt-key add -
+    echo "deb http://packages.blackfire.io/debian any main" | sudo tee /etc/apt/sources.list.d/blackfire.list
+    sudo apt-get update
+    sudo apt-get install blackfire-agent
+    sudo blackfire-agent -register
+    sudo /etc/init.d/blackfire-agent restart
+    sudo apt-get install blackfire-agent
+    blackfire config
+    sudo apt-get install blackfire-php
+    
+    ");
+
+}
