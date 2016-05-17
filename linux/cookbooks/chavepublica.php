3aeba7b48bb2f23be95f5291ad3a945aca4df9a4
jeanrodrigues <jean.rodrigues@superlogica.com>
2016-05-17 17:02:45 -0300

    alterações chavepublica

--- linux/cookbooks/chavepublica.php
+++ linux/cookbooks/chavepublica.php
@@ -3,17 +3,17 @@ function chavepublica_init(){
     
     $usuario = `echo \$SUDO_USER`;
     
-    if (!is_file(`/home/$usuario/id_dsa.pub`)){
+    if (!is_file("`/home/$usuario/id_dsa.pub`")){
    
-     exec("
+     exec_script("
      su $usuario
      ssh-keygen -t dsa -N '';
      cp /home/$usuario/.ssh/id_dsa.pub $usuario.pub;
-     smbclient //SLNAS2/chaves -c 'put $usuario'.pub'");
+     smbclient //SLNAS2/chaves -c 'put $usuario.pub'");
      
     }  
     
-    exec("cp /home/$usuario/.ssh/id_dsa.pub  /home/$usuario/.ssh/$usuario.pub
+    exec_script("cp /home/$usuario/.ssh/id_dsa.pub  /home/$usuario/.ssh/$usuario.pub
        smbclient //SLNAS2/chaves -c 'put $usuario.pub'");
    
 }
