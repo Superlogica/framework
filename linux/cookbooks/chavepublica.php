7186dd35bd22eb5a0fc2ff2acb227429ca1120e6
jeanrodrigues <jean.rodrigues@superlogica.com>
2016-05-17 16:38:07 -0300

    correção localserver2

--- linux/cookbooks/chavepublica.php
+++ linux/cookbooks/chavepublica.php
@@ -3,17 +3,17 @@ function chavepublica_init(){
     
     $usuario = `echo \$SUDO_USER`;
     
-    if (!is_file("/home/$usuario/id_dsa.pub")){
+    if (!is_file(`/home/$usuario/id_dsa.pub`)){
    
-     exec_script("
-     su \$SUDO_USER\
+     exec("
+     su $usuario
      ssh-keygen -t dsa -N '';
-     cp \$HOME\/.ssh/id_dsa.pub \$USER\.pub;
-     smbclient //SLNAS2/chaves -c 'put '\$USER'.pub'");
+     cp /home/$usuario/.ssh/id_dsa.pub $usuario.pub;
+     smbclient //SLNAS2/chaves -c 'put $usuario'.pub'");
      
     }  
     
-    exec_script("cp /home/$usuario/.ssh/id_dsa.pub  /home/$usuario/.ssh/$usuario.pub
+    exec("cp /home/$usuario/.ssh/id_dsa.pub  /home/$usuario/.ssh/$usuario.pub
        smbclient //SLNAS2/chaves -c 'put $usuario.pub'");
    
 }
