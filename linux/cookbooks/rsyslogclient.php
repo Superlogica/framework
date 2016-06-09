8bdab23d14f24e85d24545b9a7bd20e53f84230a
carlossuperlogica <carlos@superlogica.com>
2016-06-08 23:24:16 -0300

    Update rsyslogclient.php

--- linux/cookbooks/rsyslogclient.php
+++ linux/cookbooks/rsyslogclient.php
@@ -7,7 +7,7 @@ function rsyslogclient_init($ip=''){
     echo "************ ERRRRRRO00000000000000000  ***************\n";
     return false;
     }
-    put_template("rsyslog_client","/etc/rsyslog.d/20-superlogica.conf",array('ip'=>$ip));      
+    put_template("rsyslog_client","/etc/rsyslog.d/20-superlogica.conf",array('{ip}'=>$ip));      
     exec_script('sudo service rsyslog restart');
     
 }
