f00ad4e9235acb98d59de8956a37d26d06f7c45a
carlossuperlogica <carlos@superlogica.com>
2016-06-08 23:22:27 -0300

    Update rsyslogclient.php

--- linux/cookbooks/rsyslogclient.php
+++ linux/cookbooks/rsyslogclient.php
@@ -2,15 +2,12 @@
 function rsyslogclient_init($ip=''){
     
     if (!$ip){
-    echo "************ ERRRRRRO00000000000000000  ***************";
-    echo "************ Syslog incorreto ip vazio  ***************";
-    echo "************ ERRRRRRO00000000000000000  ***************";
+    echo "************ ERRRRRRO00000000000000000  ***************\n";
+    echo "************ Syslog incorreto ip vazio  ***************\n";
+    echo "************ ERRRRRRO00000000000000000  ***************\n";
     return false;
     }
-          
-    exec_script('
-    sudo rm /etc/rsyslog.d/20-superlogica.conf
-    echo "*.*  @'.$ip.':514" > /etc/rsyslog.d/20-superlogica.conf 
-                service rsyslog restart');
+    put_template("rsyslog_client","/etc/rsyslog.d/20-superlogica.conf",array('ip'=>$ip));      
+    exec_script('sudo service rsyslog restart');
     
 }
