f98443b86feac0df037c7e06d7e28577d5fa3564
Marcos Martins <marcosrcmartins@gmail.com>
2016-06-14 11:56:03 -0300

    Aide instalador

---
+++ linux/cookbooks/aide.php
@@ -0,0 +1,10 @@
+<?php
+function localserver_init(){
+   exec_script("
+        apt-get autoclean
+        apt-get autoremove
+        apt-get update
+        apt-get -y install aide
+    ");
+   put_template("local/aide.conf","/etc/aide/aide.conf");
+}
