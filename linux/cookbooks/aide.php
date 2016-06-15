16fe40d879940403ddd325b9618a66e9c736b2f9
Marcos Martins <marcosrcmartins@gmail.com>
2016-06-15 10:06:27 -0300

    AJUSTES

--- linux/cookbooks/aide.php
+++ linux/cookbooks/aide.php
@@ -1,10 +1,12 @@
 <?php
-function localserver_init(){
+function aide_init(){
+   put_template("aide.conf","/etc/aide/aide.conf");
    exec_script("
         apt-get autoclean
         apt-get autoremove
         apt-get update
         apt-get -y install aide
+        aideinit
+
     ");
-   put_template("aide.conf","/etc/aide/aide.conf");
 }
