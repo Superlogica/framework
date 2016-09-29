cf6778ac03955dc9cc7ddf4d60ebb599033f13e9
jeanrodrigues <jean.rodrigues@superlogica.com>
2016-09-29 14:01:00 -0300

    Alterações conectar admin

---
+++ linux/cookbooks/usuariosinfraamazon.php
@@ -0,0 +1,36 @@
+<?php
+function usuariosinfraamazon_init(){
+
+	exec_script("
+
+		#Criação de grupos
+
+		addgroup infra;
+		addgroup usuarios;
+		addgroup dev;
+		addgroup dev-admin;
+		addgroup suporte-admin;
+		addgroup subad-admin
+				
+		#Criação de usuarios
+
+		sudo cloud-init usuario sljumpserver infra;
+		sudo cloud-init usuario jeanrodrigues-sljumpserver infra;
+		sudo cloud-init usuario matheus-sljumpserver infra;
+		sudo cloud-init usuario luis-sljumpserver infra;
+		sudo cloud-init usuario carlos-sljumpserver infra;
+
+		#Adicionar ao sudo
+
+		adduser sljumpserver sudo;
+		adduser jeanrodrigues-sljumpserver sudo;
+		adduser matheus-sljumpserver sudo;
+		adduser luis-sljumpserver sudo;
+		adduser carlos-sljumpserver sudo;
+
+        
+    ");
+
+
+
+}
