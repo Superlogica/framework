cb1299614d9c759cc14c6f306c0f0efb3d5272cb
Matheus Fidelis <msfidelis@users.noreply.github.com>
2017-01-31 11:50:19 -0200

    Update blackfire.php

--- linux/cookbooks/blackfire.php
+++ linux/cookbooks/blackfire.php
@@ -11,17 +11,14 @@ function blackfire_init(){
           
           
     exec_script("
-    
     wget -O - https://packagecloud.io/gpg.key | sudo apt-key add -
     echo 'deb http://packages.blackfire.io/debian any main' | sudo tee /etc/apt/sources.list.d/blackfire.list
     sudo apt-get update
-    sudo apt-get install blackfire-agent
+    sudo apt-get install blackfire-agent blackfire-php -y
     sudo blackfire-agent -register
     sudo /etc/init.d/blackfire-agent restart
     sudo apt-get install blackfire-agent
-    blackfire config
-    sudo apt-get install blackfire-php
-    
+    blackfire config    
     ");
 
 }
